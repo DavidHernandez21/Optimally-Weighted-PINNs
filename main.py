cccf11ca06e0140f12d9fba0ee8bee5a31a9e19f
DavidHernandez21 <55497518+DavidHernandez21@users.noreply.github.com>
2020-12-24 18:48:47 +0100

    Update main.py
    comment backend setting

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import matplotlib
-matplotlib.use("Qt5Agg")
+# import matplotlib
+# matplotlib.use("Qt5Agg")
 from matplotlib import pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from matplotlib import rc
