70ee72b1bf24ad0d6d1fbd5b75e79aa2a90dcad3
DavidHernandez21 <55497518+DavidHernandez21@users.noreply.github.com>
2020-12-25 17:45:21 +0100

    Update main_laplace_2d.py
    added csv writter function

--- Scripts/main_laplace_2d.py
+++ Scripts/main_laplace_2d.py
@@ -4,6 +4,7 @@ from mpl_toolkits.mplot3d import Axes3D
 from NeuralNetwork import NeuralNetwork
 from PDEs.Laplace_2d import Laplace_2d
 from Enums import ActivationFunction, TrainMode
+from csv_writter.write_csv_for_plotting import csv_plot_3d_or_2d
 
 
 def main():
@@ -32,6 +33,7 @@ def main():
     fig = plt.figure()
     ax = fig.gca(projection='3d')
     x, y, z = laplace.GetInteriorPlotData(pointCount=5000, tensor=network.yInt, x=[(0, 1), (0, 1)])
+    csv_plot_3d_or_2d(x, y, z=z, csv_name='surface_plot_3d.csv')
     ax.plot_surface(x, y, z, cmap='viridis')
     ax.set_xlabel("$x_1$")
     ax.set_ylabel("$x_2$")
@@ -40,6 +42,7 @@ def main():
     # Boundary conditions
     color = next(ax._get_lines.prop_cycler)['color']
     x, y, z = laplace.GetBoundaryPlotData(pointCount=1000, tensor=network.boundaryCondition, x=[(0, 1), (0, 1)])
+    csv_plot_3d_or_2d(x, y, z=z, csv_name='boundary_plot_3d.csv')
     ax.plot3D(x[0], y[0], z[0], color=color, label="Boundary Condition")
     for i in range(1, len(x)):
         ax.plot3D(x[i], y[i], z[i], color=color)
@@ -47,10 +50,12 @@ def main():
     # Single boundary plot
     plt.figure()
     x, y = laplace.GetInteriorPlotData(pointCount=1000, tensor=network.yInt, x=[(0, 1), 0.25])
+    csv_plot_3d_or_2d(x, y, csv_name='interior_plot_2d.csv', single_plot=True)
     plt.plot(x, y, label="Approximated Solution")
 
     # Boundary conditions
     x, y = laplace.GetBoundaryPlotData(pointCount=1000, tensor=network.boundaryCondition, x=[(0, 1), 0.25])
+    csv_plot_3d_or_2d(x, y, csv_name='boundary_plot_2d.csv', single_plot=True)
     plt.scatter(x, y, label="Boundary Condition")
 
     plt.xlabel("$x_1$")
